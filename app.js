90451bb2a96a648156bc6adc577f3b56335708a4
WorkVanshika <vanshikashah.work@gmail.com>
2025-06-16 00:31:48 +0530

    feat(backend): added Mongodb connection logic in app.js

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const usersRouter = require("./routes/usersRoute");
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.use(express.json());
+app.use("/api/v1/users", usersRouter);
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('Something bad happened', err);
-    }
-    console.log(`Server is listening on ${port}`);
-});
-
-
+mongoose
+  .connect(process.env.MONGODB_URI, {})
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log("Server started on port", PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
 
 module.exports = app;
